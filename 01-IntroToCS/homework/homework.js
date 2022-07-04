cd546411eecf9afdd4da5ec37e9928b730069c6c
Elbongo <nico.silva.97@hotmail.com>
2022-07-04 18:25:47 -0300

    Homework CS finalizada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,22 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let resultado = 0;
+  for (let i = 0; i < num.length; i++) {
+    let numero = num[i] * (Math.pow(2, (num.length - i - 1)));
+    resultado += numero;
+  }
+  return resultado;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let arr = [];
+  while (num >= 1) {
+    arr.unshift(num % 2 + "");
+    num = Math.floor(num / 2);
+  }
+  return arr.join("");
 }
 
 
